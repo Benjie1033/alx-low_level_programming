7d6e5edb99e012b261575497a1ce430845bd3cc7
Benjie1033 <benjaminmugo100@gmail.com>
2023-06-19 15:08:13 -0700

    this is the start

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
 /**
- *main - The variable n will store a different value every time you will run this program
+ *main - The variable n stores a different value each time the program runs
  *Return: 0 (pass)
  */
 int main(void)
 {
 	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+
+	if (n > 0 || n < 0 || n == 0)
+	{
+		printf("positive\n");
+		printf("is negative\n");
+		printf("is zero\n");
+	}
 	return (0);
 }
