e1509f4bbace3a6418dac47b893a91bc1725ae62
Benjie1033 <benjaminmugo100@gmail.com>
2023-06-22 00:34:45 -0700

    loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+# include "main.h"
+
+/**
+ * print_alphabet - all alphabets printed in lowercase letters
+ */
+void print_alphabet(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	alph++;
+
+		_putchar(alph);
+		_putchar('\n');
+}
