ba489bc9085330a55a5b862bff7afc55ea3a6762
Benjie1033 <benjaminmugo100@gmail.com>
2023-07-18 14:00:24 -0700

    correction

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,11 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+/**
+ * main - main function
+ * Return: 0
+ */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,2 +1,6 @@
+# ifndef FUNCTION_LIKE_MACRO_H
+# define FUNCTION_LIKE_MACRO_H
+
 # define ABS(x) x
 
+#endif
