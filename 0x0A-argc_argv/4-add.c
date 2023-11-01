d822b937720ba928e50f79ea4f218dee73a4b93f
Benjie1033 <benjaminmugo100@gmail.com>
2023-11-01 14:40:22 -0700

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -26,37 +26,44 @@ int check_num(char *str)
 	}
 
 /**
- * main - name of the program
- * @argc: Counts arguments
- * @argv: Argument
+ * main - main function
+ * @argc: Argument count
+ * @argv: Argument vector
  *
- * Return: 0 (Success)
+ * Return: Always 0
  */
 
-int main(int argc, char *argv[])
-{
-int count;
-int str_to_int;
-int sum = 0;
-
-count = 1;
-
-while (count < argc)
-{
-if (check_num(argv[count]))
-
+	int main(int argc, char *argv[])
 	{
-		str_to_int = atoi(argv[count]);
-		sum += str_to_int;
-	}
-else
-	{
-		printf("Error\n");
-	return (1);
-	}
-		count++;
-	}
-	printf("%d\n", sum);
-	return (0);
+		int count;
+		int str_to_int;
+		int sum = 0;
+
+
+		count = 1;
+		while (count < argc)
+		{
+			if (check_num(argv[count]))
+
+
+			{
+				str_to_int = atoi(argv[count]);
+				sum += str_to_int;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+
+			count++;
+		}
+
+
+		printf("%d\n", sum); /*print sum*/
+
+
+		return (0);
 	}
 
